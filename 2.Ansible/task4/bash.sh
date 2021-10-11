a901c87bb7c878eb1429b62bc96957f5f850e865
Max <maxonchik@bk.ru>
2021-10-11 22:38:46 +0300

    task 4

--- 2.Ansible/task4/bash.sh
+++ 2.Ansible/task4/bash.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo && yum install -y docker-ce docker-ce-cli containerd.io && systemctl enable --now docker && usermod -aG docker $(whoami) && newgrp docker && ln -sf /usr/bin/python3 /usr/bin/python && pip3 install docker-compose && setenforce 0
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo && \
+yum install -y docker-ce docker-ce-cli containerd.io && systemctl enable --now docker && \
+usermod -aG docker $(whoami) && usermod -aG docker vagrant && \
+newgrp docker && ln -sf /usr/bin/python3 /usr/bin/python && pip3 install docker-compose && setenforce 0
