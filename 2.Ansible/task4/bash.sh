d9fdad0efb977ae690119ae2f00564df0148fc43
Max <maxonchik@bk.ru>
2021-10-11 18:53:05 +0300

    task 4

---
+++ 2.Ansible/task4/bash.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo && yum install -y docker-ce docker-ce-cli containerd.io && systemctl enable --now docker && usermod -aG docker $(whoami) && newgrp docker && ln -sf /usr/bin/python3 /usr/bin/python && pip3 install docker-compose && setenforce 0
